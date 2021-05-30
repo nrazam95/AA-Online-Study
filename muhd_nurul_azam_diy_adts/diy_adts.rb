8ecb1c22a2a77ea13d90cbb25df56fec4cfe8594
nrazam95 <nrazam95@gmail.com>
2021-05-30 14:05:23 +0800

    adding Stack class

---
+++ muhd_nurul_azam_diy_adts/diy_adts.rb
@@ -0,0 +1,20 @@
+class Stack
+
+    def initialize
+        
+    end
+    
+    def push(el)
+        
+    end
+    
+    def pop
+        
+    end
+
+    def peek
+        
+    end
+    
+    
+end
